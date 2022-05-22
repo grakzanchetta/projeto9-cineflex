598121ffe39cb9f960714ea575a6e842eca22e41
Gabriel Rak <grakzanchetta@gmail.com>
2022-05-22 18:55:00 -0300

    Esqueleto das telas feito

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Principal from "./Principal";
+import Sessoes from "./Sessoes";
+import Assentos from "./Assentos";
+import Sucesso from "./Sucesso";
+
 export default function App (){
     return(
-        <h1>Olá!!!</h1>
+        <BrowserRouter>
+        <h1>Header</h1>
+        <Routes>
+            <Route path="/" element={Principal}/>
+            <Route path="/sessoes/:idFilme" element={Sessoes}/>
+            <Route path="/assentos/:idSessao" element={Assentos}/>
+            <Route path="/sucesso" element={Sucesso}/>
+        </Routes>
+        </BrowserRouter>
     )
 }
